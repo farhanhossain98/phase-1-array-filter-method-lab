7fbe177204cd7ea6c5a481c00ff8da52c080a71a
farhan hossain <farhanrufon99@gmail.com>
2023-07-15 15:25:20 -0400

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+const findMatching = (names,passed) => {
+    return names.filter((element) => element.toUpperCase() === passed.toUpperCase())
+
+}
+
+
+
+const fuzzyMatch = (names,firstLetters) => {
+    return names.filter((element) => element.toUpperCase().indexOf(firstLetters[0].toUpperCase()) === 0)
+}
+
+const matchName = (names, match) => {
+    return names.filter((element) => element.name === match)
+}
